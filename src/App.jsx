0fbfe390c2c83f3b3c0ba594adbc65aa2dda4eb6
rrtyagii <rtyagi1@hawk.iit.edu>
2023-11-05 22:09:27 -0600

    added avatar commit, changing profile-header to include use avatar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import TabList from "./components/Tab/TweetLists";
+import Avatar from "./components/Avatar/Avatar";
+import UserAvatar from "./images/user-avatar.svg"
+import ProfileHeader from "./components/Profile-Header/ProfileHeader";
 
 export default function App() {
   return (
     <>
     <div className="min-h-screen bg-black"> 
-      <TabList />
+      {/* <Avatar 
+        imageUrl={UserAvatar}
+        userName = {"aaray"}
+        userHandle = {"Aarya"}
+        showNameAndHandle = {true}
+      /> */}
+
+<ProfileHeader 
+  props={{
+    userBackground: UserAvatar, // assuming this is a variable
+    userImage: UserAvatar, // assuming this is a variable
+    userFullName: "hello hello",
+    userName: "hellohello",
+    bio: "This is the bio text.",
+    bioLink: "http://example.com",
+    joinedAt: "September 2018",
+    following: "213",
+    followers: "118",
+  }}
+/>
+
+
     </div>
     </>
   ); 
--- src/components/Profile-Header/ProfileHeader.jsx
+++ src/components/Profile-Header/ProfileHeader.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import Avatar from "../Avatar/Avatar";
 
 export default function ProfileHeader({props}){
+    const {
+        userBackground,
+        userImage,
+        userFullName,
+        userName,
+        bio,
+        bioLink,
+        joinedAt,
+        following,
+        followers
+    } = props;
+
     return (
         <>
             <section>
-            <figure className="w-24.375rem h-[9.375rem] flex-shrink-0 bg-cover bg-no-repeat bg-custom-gray bg-custom-position">
-                <img src={props.userBackground} alt="profile-cover" />
-            </figure>
+                <figure className="w-24.375rem h-[9.375rem] flex-shrink-0 bg-cover bg-no-repeat bg-custom-gray bg-custom-position">
+                    <img src={userBackground} alt="profile-cover" />
+                </figure>
             </section>
 
             <article className="relative ml-5">
-            <figure className="w-20 h-20 shrink-0 rounded-12.5rem border-4 border-neutral-1000 bg-cover bg-no-repeat bg-custom-gray bg-custom-position absolute top-[-2.5rem]">
-                <img className="w-20 h-20" src={props.userImage} alt="User Avatar" />
-            </figure>
+            <Avatar 
+                imageUrl={userImage}
+                userName={userFullName}
+                userHandle={userName.substring(1)}
+                orientation="vertical"
+                borderRadius ="rounded-12.5rem"
+                size="w-20 h-20"
+
+
+            />
             </article>
 
             <button class="absolute top-40 right-4 inline-flex w-32 px-5 py-2 justify-center items-center gap-2.5 rounded-4xl border border-custom-stroke shadow-lc backdrop-blur-2xl bg-neutral-1000">
@@ -21,65 +41,6 @@ export default function ProfileHeader({props}){
                 </div>
             </button>
 
-            <article className="flex w-22.5rem flex-col justify-end items-start gap-4 ml-6 mr-4 mt-14">
-
-                <section className="flex flex-col justify-end items-start gap-1">
-                    <div className="font-inter text-xl text-neutral-50 leading-normal font-bold">
-                    {props.userFullName}
-                    </div>
-                    <div className="font-inter text-[0.9375rem] font-normal leading-normal text-neutral-500">
-                    {props.userName}
-                    </div>
-                </section>
-
-                <section className="font-inter text-base font-normal leading-normal text-neutral-50">
-                    {props.bio}
-                </section>
-
-                <section className="flex flex-row items-start gap-5">
-                    <div className="flex items-center gap-1">
-
-                        <figure className="w-3.5 h-3.5 fill-neutral-500">
-                            <img src="/public/images/link-icon.svg" alt="link"/>
-                        </figure>
-
-                        <div className="text-twitter-blue-default font-inter font-normal leading-normal text-base">
-                            {props.bioLink}
-                        </div>
-                    </div>
-
-                    <div className="flex items-center gap-1">
-                        <figure className="w-3.5 h-3.5 fill-neutral-500">
-                            <img src="/public/images/calendar-icon.svg" alt="calendar" />
-                        </figure>
-
-                        <div className="text-neutral-500 font-inter font-normal leading-normal text-base">
-                            {props.joinedAt}
-                        </div>
-                    </div>
-
-                </section>
-
-                <section className="flex flex-row items-start gap-5">
-                    <div className="flex items-center gap-1">
-                        <div className="text-neutral-50 font-inter font-bold leading-normal text-base">
-                           {props.following}
-                        </div>
-                        <div className="text-neutral-500 font-inter font-normal leading-normal text-base">
-                            Following
-                        </div>
-                    </div>
-
-                    <div className="flex items-center gap-1">
-                        <div className="text-neutral-50 font-inter font-bold leading-normal text-base">
-                            {props.followers}
-                        </div>
-                        <div className="text-neutral-500 font-inter font-normal leading-normal text-base">
-                            Followers
-                        </div>
-                    </div>
-                </section>
-            </article>
         </>
     ); 
 
