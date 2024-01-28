778ba96a523cddfcd5c0bcf4d659e064cd9f612d
rrtyagii <rtyagi1@hawk.iit.edu>
2024-01-28 17:54:23 -0600

    added like functionality, fetchiing feed and following feed fetching functionality

--- src/page/Home Feed/HomeFeed.jsx
+++ src/page/Home Feed/HomeFeed.jsx
@@ -1,18 +1,22 @@
-import PageHeader from '../../components/Page Header/PageHeader'; // Path: src/components/PageHeader/PageHeader.jsx
-import Avatar from '../../components/Avatar/Avatar'; // Path: src/components/Avatar/Avatar.jsx
-import TweetLists from '../../components/Tab/TweetLists'; // Path: src/components/Tab/TweetLists.jsx 
-import Post from '../../components/TweetPost/Post'; // Path: src/components/TweetPost/Post.jsx w-24.375rem
-import NavigationTab from '../../components/Navigation Tab/Navigation_Tab'; // Path: src/components/Navigation/NavigationTab.jsx
-import AddContent from '../../components/Add Content/AddContent'; // Path: src/components/Add Content/AddContent.jsx
+import {useState, useEffect, useContext} from "react";
+import { Link, useNavigate} from 'react-router-dom';
+import axios from "axios";
 
+import PageHeader from '../../components/Page Header/PageHeader'; 
+import Tab from "../../components/Tab/Tab";
+import Avatar from '../../components/Avatar/Avatar'; 
+import Post from '../../components/TweetPost/Post'; 
+import NavigationTab from '../../components/Navigation Tab/Navigation_Tab'; 
+import AddContent from '../../components/Add Content/AddContent';
+
+import { TweetContext } from '../../contexts/TweetContext';
 import Logo100 from '../../assets/login-100.svg'; 
 import LogoX from '../../assets/login-group-27162.svg';
 import avatar from '../../assets/tweet-1-user-avatar.svg';
 
-import { useContext } from 'react';
-import { TweetContext } from '../../contexts/tweetContext';
-import { Link, useNavigate} from 'react-router-dom';
 import { URLs } from '../../Constants';
+import { timeSince } from "../../utils/date_format";
+
 
 const LoginHeader = () => {
     return( 
@@ -27,11 +31,87 @@ const LoginHeader = () => {
 
 export default function HomeFeed() {
     const navigate = useNavigate();
-    const {userTweets, handleLike, handleRetweet} = useContext(TweetContext);
+    const [feed, setFeed] = useState([]);
+    const [followingFeed, setFollowingFeed] = useState([]);
+    const [currentActive, setCurrentActive] = useState("For You");
+    const [likeStatuses, setLikeStatuses] = useState({});
+
+    const getFeed = async () => {
+        try {
+            const response = await fetch("http://localhost:5050/feed",{
+                credentials: 'include'
+            });
+            const data = await response.json();
+            setFeed(data);
+
+            const likeStatuses = data.reduce((statuses, post) => {
+                statuses[post.id] = post.is_liked === 1; // Ensure boolean conversion
+                return statuses;
+            }, {});
+    
+            setLikeStatuses(likeStatuses);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getFollowingFeed = async () => {
+        try {
+            const response = await fetch("http://localhost:5050/feed/following", {
+                credentials: 'include'
+            });
+            const data = await response.json();
+            setFollowingFeed(data);
+
+            setLikeStatuses(data.reduce((statuses, post) => {
+                statuses[post.id] = post.is_liked === 1; // Ensure boolean conversion
+                return statuses;
+            }, {}));
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        if (currentActive === "For You") {
+            getFeed();
+        } else {
+            getFollowingFeed();
+        }
+    }, [currentActive]);
 
     function handleAddContentClick() {
         navigate(URLs.compose);
     }
+
+    function handleTabClick(tabName) {
+        setCurrentActive(tabName);
+    }
+
+    const handleLike = async (postId) => {
+        try {
+
+            const response = await axios.put(
+                'http://localhost:5050/post/like',
+                {
+                    post_id: postId,
+                    is_liked: !likeStatuses[postId],
+                },
+                {
+                    withCredentials: true,
+                }
+            );
+
+            setLikeStatuses(prevStatuses => ({
+                ...prevStatuses,
+                [postId]: response.data.has_liked === 1, // Ensure boolean conversion
+            }));
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
   return (
     <>   
@@ -53,36 +133,44 @@ export default function HomeFeed() {
                 </div>
             </div>
 
-            <div className='pb-3'>
-                <TweetLists/>
-            </div>
+            <nav className="w-full flex px-20 py-5 justify-center items-center self-stretch border-b border-b-neutral-700 gap-20">
+                <Tab tabName="For You" activeTab={currentActive} onClick={handleTabClick} />
+                <Tab tabName="Following" activeTab={currentActive} onClick={handleTabClick} />
+            </nav>
+
 
             <div className="flex flex-col w-full px-[0.0625] items-start relative">
                 {
-                    userTweets.map((tweet) => {
+                    (currentActive === "For You" ? feed : followingFeed)?.length > 0 ?
+                    (currentActive === "For You" ? feed : followingFeed).map((post) => {
                         const meta = {
-                            comments: tweet.comments, 
-                            reposts: tweet.reposts, 
-                            likes: tweet.likes, 
-                            views: tweet.views,
-                            isLiked: tweet.isLiked,
-                            isRetweeted: tweet.isRetweeted
+                            comments: post.replies_count, 
+                            reposts: post.repost_count, 
+                            likes: post.likes_count, 
+                            views: post.views,
+                            isLiked: likeStatuses[post.id] || false,
+                            isRetweeted: post.isRetweeted
                         }; 
-                        const post = {
-                            id: tweet.id, 
-                            text: tweet.text, 
-                            postedAt: tweet.postedAt, 
-                            postedBy: tweet.postedBy
+                        const postInfo = {
+                            id: post.id, 
+                            text: post.content, 
+                            postedAt: timeSince(post.posted_at), 
+                            postedBy: {
+                                userId: post.User.id,
+                                userName: post.User.user_name, 
+                                userFullName: post.User.display_name,
+                                userImage: post.User.profile_picture_url
+                            }
                         };
                         return (
                         <Post 
-                            key={tweet.id}
+                            key={post.id}
                             meta={meta} 
-                            post={post} 
-                            onLike={() => handleLike(tweet.id)}
-                            onRetweet={() => handleRetweet(tweet.id)}
+                            post={postInfo} 
+                            onLike={() => handleLike(Number(post.id))}
+                            onRetweet={() => handleRetweet(post.id)}
                         />)
-                    })
+                    }) : <p>No posts to display</p>
                 }
             </div>
         </div>
