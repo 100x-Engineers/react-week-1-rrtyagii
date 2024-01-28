d7ca4a2ae632f56622de713d7a05b2b0ecce5cc4
rrtyagii <rtyagi1@hawk.iit.edu>
2024-01-28 17:53:39 -0600

    added timeSince() util function

--- src/utils/date_format.js
+++ src/utils/date_format.js
@@ -9,3 +9,25 @@ export const dateFormat = (form) => {
     const displayDate = formatDate(dateOfBirth);
     return displayDate
 }
+
+export const timeSince = (timestamp) => {
+    const now = new Date();
+    const timestampDate = new Date(timestamp);
+    const secondsPast = (now.getTime() - timestampDate.getTime()) / 1000;
+
+    if(secondsPast < 3600) {
+        return Math.floor(secondsPast / 60) + 'm';
+    }
+    if(secondsPast < 86400) {
+        return Math.floor(secondsPast / 3600) + 'h';
+    }
+    if(secondsPast < 604800) {
+        return Math.floor(secondsPast / 86400) + 'd';
+    }
+
+    const day = timestampDate.getDate();
+    const month = timestampDate.getMonth() + 1; // JavaScript months are 0-based
+    const year = timestampDate.getFullYear().toString().substr(-2); // Get last two digits of year
+
+    return `${month}/${day}/${year}`;
+}
